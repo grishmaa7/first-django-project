99bd22d324b9e0a4dcb473a28487ec65b11f347e
grishmaa7 <gisme7@gmail.com>
2026-08-11 10:52:21 +0545

    more updates

--- api/serializers.py
+++ api/serializers.py
@@ -12,6 +12,18 @@ class ClassSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "description", "start_date", "end_date"]
 
 
+    #objject level validation
+    def validate(self, data):
+        start_date = data.get("start_date")
+        end_date = data.get("end_date")
+
+        if start_date and end_date and start_date > end_date:
+            raise serializers.ValidationError(
+                "Start date must be before end date."
+            )
+        return data  #always return it
+
+
 class StudentSerializer(serializers.ModelSerializer):
     # Nested (read-only), shows the whole class object when reading.
     class_detail = ClassSerializer(source="class_name", read_only=True)
@@ -30,9 +42,20 @@ class StudentSerializer(serializers.ModelSerializer):
             "class_detail",
             "enrollment_date",
         ]
-
+    def validate_age(self, value):
+        if value < 5:
+            raise serializers.ValidationError("Age must be at least 5.")
+        return value  #always return it
         # The server controls these -- clients can't set them.
         read_only_fields = ["id", "enrollment_date"]
+
+    name = serializers.CharField(
+        required=True, min_length=2,
+        error_messages={
+            "required": "Name is required.",
+            "min_length": "Name must be at least 2 characters long.",
+        },
+    )
 
 
 class TeacherSerializer(serializers.ModelSerializer):
